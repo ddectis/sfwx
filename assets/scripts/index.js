11b90f7ec384926013e8f4a7f6348817e77b4b3c
Dan Dectis <dandectis@gmail.com>
2023-07-09 22:41:09 -0700

    added ability to filter days as well as some stats

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -25,7 +25,13 @@ class PullLocalWeather {
                 
                 const lastUpdated = document.querySelector("#last-updated") //grab a place on the page to place the time that the forecast was updated
                 let updateTime = data.properties.updated.split("T");
-                lastUpdated.textContent = `Forecast Data Last Updated: ${updateTime[0]} @ ${updateTime[1].substring(0,5)}`
+                lastUpdated.textContent = `Forecast Data Last Updated: ${updateTime[0]} @ ${updateTime[1].substring(0, 5)}`
+
+                let countOfBlueHours = 0 //keep track of how many of the forecast hours are "blue" i.e. good conditions
+                let streakOfBlueHours = 0; //every consecutive blue hour adds 1 to the streak, a nonblue hour sets it back to 0
+                let longestBlueHourStreak = 0; 
+                let dayWithLongestBlueStreak = ``;
+                let totalHours = 0;
                 
                 periods.forEach(period => {         //iterate over each period
                     if (period.dewpoint !== null) { //check to ensure that the dewpoint value is not null. ONly try to create an add an object when it is not null
@@ -43,19 +49,25 @@ class PullLocalWeather {
                         if (windSpeed < windThreshold) {
                             //console.log("Possibly good conditions?");
                             if (tempDewDelta > dewDeltaThreshold) {
-                                console.log("Good Conditions!");
-                                goodConditions = true;
-                            }
-                        }  
-                            
-                        
 
+                                goodConditions = true;
+                                countOfBlueHours++;
+                                streakOfBlueHours++;
+                                if (streakOfBlueHours > longestBlueHourStreak) {
+                                    dayWithLongestBlueStreak = this.getDayOfWeek(entryTime[0].substring(5))
+                                    longestBlueHourStreak = streakOfBlueHours;
+                                }
+                                //console.log("Good Conditions! Count = " + countOfBlueHours + " streak count: " + longestBlueHourStreak + " Longest streak ends on: " + dayWithLongestBlueStreak);
+
+                            } else {
+                                streakOfBlueHours = 0;
+                            }
+                        }
 
                         if (windSpeed < 10) { //insert a leading zero for a wind < 10
                             windSpeed = "0" + windSpeed; //windSpeed is converted to a string at this point!
                             //console.log("We appended a 0 to the front of a < 10 windspeed and got: " + windSpeed)
                         }
-
 
                         //create a new object for each iteration
                         let obj = {                 
@@ -71,14 +83,49 @@ class PullLocalWeather {
                         weatherObjects.push(obj);   //add the new object to the array that stores the forecast objects
                     }
                     
-                    
+                    totalHours++;
                     return weatherObjects;
                 })
                 console.log("Weather Objects Follow");
                 //console.dir(weatherObjects);
+                this.printSummary(countOfBlueHours, dayWithLongestBlueStreak, longestBlueHourStreak, totalHours)
                 this.printCast(weatherObjects);
                 return weatherObjects;
             }).catch(error => console.log(error));
+    }
+
+    getDayOfWeek = numericalDate => {
+        //given the current date, calculate a day of the week
+        let currentYear = new Date().getFullYear(); //first find the year          
+
+        let dateObj = new Date(currentYear + "-" + numericalDate); //then great a Date Object 
+        let timezoneOffeset = dateObj.getTimezoneOffset(); //figure out the timezone offset in the user's timezone
+
+        dateObj.setMinutes(dateObj.getMinutes() + timezoneOffeset) //adjust the minutes in that Date object to account for that offset. 
+        //Failure to do this will e.g.lead to data from PDT showing up as 17: 00 on the day before because 24 - 7(the offset at time of writing) = 17!
+        let date = dateObj.toString(); //convert the date object to a string so that you can take a substring from it
+
+        //console.log("Date is: " + dateObj + " Logged Date is: " + object.date + " Logged Time is: " + object.time)
+        let dayOfWeek = date.substring(0, 3); //grab just the first 3 characters of the date string e.g. SUN, MON etc
+        return dayOfWeek
+    }
+
+    printSummary = (countOfBlueHours, dayWithLongestStreak, longestStreakCount, totalHours) => {
+        const weatherSummary = document.querySelector("#weather-summary");
+        let hourOrHours = "";
+        if (longestStreakCount === 1) {
+            hourOrHours = "hour"
+        } else {
+            hourOrHours = "hours"
+        }
+
+        let percentOfBlueHours = longestStreakCount / totalHours * 100
+        percentOfBlueHours = percentOfBlueHours.toFixed(1);
+        weatherSummary.insertAdjacentHTML("beforeend", `
+            <li><u>Best Looking Day:</u> On <b>${dayWithLongestStreak}</b> there will be <b>${longestStreakCount} blue ${hourOrHours}!</b></li>
+            <li>In the next <b>${totalHours}</b> hours, <b>${countOfBlueHours}</b> of them will be blue i.e. <b>${percentOfBlueHours}%</b></li>
+
+            `)
     }
 
     printCast = objects => {
@@ -112,23 +159,12 @@ class PullLocalWeather {
                 qualityCategory = 1;
             }
 
-            //given the current date, calculate a day of the week
-            let currentYear = new Date().getFullYear(); //first find the year          
-
-            let dateObj = new Date(currentYear + "-" + object.date); //then great a Date Object 
-            let timezoneOffeset = dateObj.getTimezoneOffset(); //figure out the timezone offset in the user's timezone
-
-            dateObj.setMinutes(dateObj.getMinutes() + timezoneOffeset) //adjust the minutes in that Date object to account for that offset. 
-                                                                        //Failure to do this will e.g.lead to data from PDT showing up as 17: 00 on the day before because 24 - 7(the offset at time of writing) = 17!
-            let date = dateObj.toString(); //convert the date object to a string so that you can take a substring from it
-
-            //console.log("Date is: " + dateObj + " Logged Date is: " + object.date + " Logged Time is: " + object.time)
-            let dayOfWeek = date.substring(0, 3); //grab just the first 3 characters of the date string e.g. SUN, MON etc
+            let dayOfWeek = this.getDayOfWeek(object.date);
             
 
             // Populate the cell with object data
             cell.innerHTML = `
-                <div class="weather-entry category-${qualityCategory}">
+                <div class="weather-entry category-${qualityCategory}" id="${dayOfWeek.toLowerCase()}">
                     <div class="weather-time">${dayOfWeek} ${object.date} @ ${object.time}:00</div>
                     <div class="weather-data">
                         <div class="weather-temperature">
@@ -157,8 +193,113 @@ class PullLocalWeather {
             // Append the cell to the grid container
             gridContainer.appendChild(cell);
         }
+
+        //create a new instance of the class and run the instantiate function
+        const filter = new FilterDaysOfWeek;
+        filter.instantiateFilterButtons();
+
+
     }
+
+
+
 }
+
+//using buttons on the UI, filter which days of the week are visible
+class FilterDaysOfWeek {
+    instantiateFilterButtons = () => {
+        //create references to the UI buttons we're going to use to filter the results by day of the week
+        const sundayButton = document.querySelector("#sunday");
+        const mondayButton = document.querySelector("#monday");
+        const tuesdayButton = document.querySelector("#tuesday");
+        const wednesdayButton = document.querySelector("#wednesday");
+        const thursdayButton = document.querySelector("#thursday");
+        const fridayButton = document.querySelector("#friday");
+        const saturdayButton = document.querySelector("#saturday");
+        const allButton = document.querySelector("#all-days");
+
+        //then put them all into an array
+        const filterButtons = [sundayButton, mondayButton, tuesdayButton, wednesdayButton, thursdayButton, fridayButton, saturdayButton, allButton]
+
+        //iterate over the button array and add an event listener on each one
+        filterButtons.forEach(button => {
+            button.addEventListener("click", event => {
+                //each button calls the filter method and passses through the text on the button (e.g. "Sunday")
+                this.filter(event.srcElement.innerHTML)
+            })
+        })
+
+    }
+
+    filter = day => {
+
+        //trim the passed value e.d. "Sunday" becomes "sun"
+        const trimmedFilterParam = day.toLowerCase().slice(0, 3);
+
+        //these will return a node list so we'll have to forEach over them
+        const sundays = document.querySelectorAll("#sun")
+        const mondays = document.querySelectorAll("#mon")
+        const tuesdays = document.querySelectorAll("#tue")
+        const wednesdays = document.querySelectorAll("#wed")
+        const thursdays = document.querySelectorAll("#thu")
+        const fridays = document.querySelectorAll("#fri")
+        const saturdays = document.querySelectorAll("#sat")
+
+        //and then put all all of those together in an array so you can forEach over them all at once
+        const dayGroup = [sundays, mondays, tuesdays, wednesdays, thursdays, fridays, saturdays]
+
+        //so we start by iterating over the array, each entry in that array will then also need to have its members iterated over
+        dayGroup.forEach(dayOfWeek => {
+            //start by hiding all of the days
+            dayOfWeek.forEach(day => {
+                day.classList.add("hide")
+            })
+        })
+
+        //then check the trimmed filter parameter for a match, unhide the matched days       
+        if (trimmedFilterParam === "sun") {
+            sundays.forEach(day => {
+                day.classList.remove("hide");
+            })
+        } else if (trimmedFilterParam === "mon") {
+            mondays.forEach(day => {
+                day.classList.remove("hide");
+            })
+        } else if (trimmedFilterParam === "tue") {
+            tuesdays.forEach(day => {
+                day.classList.remove("hide");
+            })
+        } else if (trimmedFilterParam === "wed") {
+            wednesdays.forEach(day => {
+                day.classList.remove("hide");
+            })
+        } else if (trimmedFilterParam === "thu") {
+            thursdays.forEach(day => {
+                day.classList.remove("hide");
+            })
+        } else if (trimmedFilterParam === "fri") {
+            fridays.forEach(day => {
+                day.classList.remove("hide");
+            })
+        } else if (trimmedFilterParam === "sat") {
+            saturdays.forEach(day => {
+                day.classList.remove("hide");
+            })
+        } else if (trimmedFilterParam === "all") {
+            dayGroup.forEach(dayOfWeek => {
+                dayOfWeek.forEach(day => {
+                    day.classList.remove("hide")
+                })
+            })
+        }
+
+    }
+    
+
+}
+
+
+   
 
 //enable this code to find a grid location
 
@@ -168,7 +309,6 @@ class PullLocalWeather {
 //pull the forecast for the configured url
 const forecast = new PullLocalWeather;
 forecast.pullCast();
-
 
 
 
